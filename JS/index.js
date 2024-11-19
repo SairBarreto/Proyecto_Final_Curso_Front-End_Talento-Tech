36447ce1bd9e0aa16c990160af52d32278d06e65
Sair Barreto <sairgregori@gmail.com>
2024-11-18 22:02:27 -0300

    Agrego Funcion en JS para añadir productos al carrito

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,53 @@ document.getElementById("toggleCarrito").addEventListener("click", function(even
   });
 */
   
+//Configuracion para que se agregen los productos al carrito
+const cardInfo = document.querySelector('.card-producto')
+const filaProducto = document.querySelector('.fila-productos')
+
+//Lista de todos los contenedores de los productos
+const productosLista = document.querySelector('#productos')
+
+//Variable de arreglos de Productos
+let todosLosProductos = []
+
+productosLista.addEventListener('click', e => {
+  
+  if(e.target.classList.contains('btn-anadir-card')){
+    const producto = e.target.parentElement
+
+    const infoProducto = {
+      cantidad: 1,
+      titulo: producto.querySelector('h2').textContent,
+      precio: producto.querySelector('p').textContent,
+    }
+
+    // El ... lo que hace es que si la lista ya tiene elementos este se añade al final
+    todosLosProductos = [...todosLosProductos, infoProducto];
+    
+    mostrarEnHTML();
+  }
+})
+
+//Funcion para mostrar en el HTML
+const mostrarEnHTML = () => {
+  
+  //Limpio el HTML para que no se agregen duplicados
+  filaProducto.innerHTML = '';
+
+  todosLosProductos.forEach(producto => {
+    const contenedorProducto = document.createElement('div');
+    contenedorProducto.classList.add('card-producto');
+
+    contenedorProducto.innerHTML = `
+    <div class="informacion-producto-card">
+        <div class="cantidad-producto-carrito">${producto.cantidad}</div>
+        <p class="titulo-producto-carrito">${producto.titulo}</p>
+        <span class="precio-producto-carrito">${producto.precio}</span>
+    </div>
+    <i class="fa-solid fa-xmark"></i>
+    `;
+
+    filaProducto.append(contenedorProducto);
+  });
+};
